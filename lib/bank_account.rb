e16be75f53526cc9eb38310c57d1b7d71dac7fb5
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-20 19:38:22 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,16 @@
 class BankAccount
+attr_accessor :name, :balance
+
+def initialize(name)
+  @name = name
+  @balance = 1000
+end
+
 
 end
+
+
+#transfer money between bank accounts through creating an instance of Transfer
+#kinda like creating a transaction
+#Transfer validates payment before sending, status starts as pending, then either completed or rejected. also a payment u cancel is "reversed" status
+#do u see how modeling objects make it so much easier to understand/code?

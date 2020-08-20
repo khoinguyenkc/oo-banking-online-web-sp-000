c557a8742805564c16d0426c62fef9e5d9c6fe62
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-20 19:42:27 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -2,18 +2,23 @@ class BankAccount
 attr_reader :name, :balance
 #u can read but not change!
 
-def initialize(name)
-  @name = name
-  @balance = 1000
-  @status = "open"
-end
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
 
-def deposit(amount)
-  @balance += amount
-end
+  def deposit(amount)
+    @balance += amount
+  end
 
-def display_balance
-  puts "Your balance is $#{self.balance}."
+  def display_balance
+    puts "Your balance is $#{self.balance}."
+  end
+
+  def close_account
+    @status = "closed"
+
 
 end
 

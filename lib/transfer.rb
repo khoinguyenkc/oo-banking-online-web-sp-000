b9b34c9fdca951154453f139f200bc58f8ce3d57
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-20 19:52:34 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,20 @@
 class Transfer
+  attr_accessor :sender, :receiver, :amount, :status
   # your code here
+
+  def initialize(sender,receiver,amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+
+
+
+
+
+
+  
 end
+

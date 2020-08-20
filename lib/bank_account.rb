5214576c3ac42e9f8b56f2cd6be6dd7696bcb101
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-20 19:46:54 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,7 +1,7 @@
 class BankAccount
-attr_reader :name,
+attr_reader :name
 #u can read but not change name!
-attr_accessor :balance
+attr_accessor :balance, :status
 
   def initialize(name)
     @name = name
